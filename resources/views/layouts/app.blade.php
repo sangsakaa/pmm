436625b7c7b50e3971f477d4f467fa359a49df85
sangsakaa <zainuri717@gmail.com>
2023-07-22 23:29:58 +0700

    color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 
     <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
     <title>PMM @yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
